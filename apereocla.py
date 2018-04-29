01522a0597806cdc8095a7a6a994a6c3b4f3f55f
Lars Kiesow <lkiesow@uos.de>
2018-04-30 01:10:23 +0200

    Minimal library
    This patch adds a minimal, but sully functional version of the library
    to access the Apereo Foundations list of completed CLAs.

---
+++ apereocla.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+'''
+python-apereocla
+================
+
+This is a simple python library to parse and access the Apereo Foundation's
+lists include those who have submitted Apereo Foundation CLAs, as well as those
+who had previously submitted CLAs with Jasig or Sakai.
+
+The data are retrieved from the official lists at:
+
+> http://licensing.apereo.org/
+
+This library offers access to the lists of ICLAs, CCLAs as well as the list of
+Github user names associated with ICLAs.
+
+:license: MIT
+'''
+
+from bs4 import BeautifulSoup
+import requests
+
+
+BASEURL = 'https://docs.google.com' \
+          '/spreadsheets/d/1RoXOkTLsa2lAMOj0PoH4zDB7Rjz-gIJqH1aFYwZBcMU' \
+          '/pubhtml/sheet?headers=false&gid='
+GHURL = BASEURL + '2009020919'
+CLAURL = BASEURL + '5'
+
+
+def github_users():
+    req = requests.get(GHURL)
+    assert req.ok
+    soup = BeautifulSoup(req.text, 'html.parser')
+    for gh_username in soup.find_all('td', **{'class': 's1'}):
+        if gh_username.string:
+            yield gh_username.string
+
+
+def _cla(field):
+    req = requests.get(CLAURL)
+    assert req.ok
+    soup = BeautifulSoup(req.text, 'html.parser')
+    for tr in soup.find_all('tr'):
+        td = tr.find_all('td', **{'class': 's3'})
+        if len(td) != 2:
+            continue
+        cla = td[field].string
+        if cla:
+            yield cla
+
+
+def icla():
+    return _cla(0)
+
+
+def ccla():
+    return _cla(1)
